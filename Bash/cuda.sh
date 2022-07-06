e01f3e13672e953a82b98fffe079ee717498406d
LuanB-compt <luan-bruno-domingues@hotmail.com>
2022-07-06 16:48:49 -0300

    Updated the files header

--- Bash/cuda.sh
+++ Bash/cuda.sh
@@ -1,4 +1,4 @@
-#CUDA INSTALATION 11.6
+#CUDA INSTALLATION 11.6
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu1804-11-6-local_11.6.0-510.39.01-1_amd64.deb
--- Bash/sdk_zed.sh
+++ Bash/sdk_zed.sh
@@ -1,4 +1,4 @@
-#SDK INSTALATION 
+#SDK INSTALLATION 
 cd ~/Downloads
 chmod +x ZED_SDK_Ubuntu18_cuda11.5_v3.7.4.run
 ./ZED_SDK_Ubuntu18_cuda11.5_v3.7.4.run
